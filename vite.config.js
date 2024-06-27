719e109fffb311bbd97db6a1e54e3302a84a7a27
Henry <a54321580@yahoo.com.tw>
2024-06-27 15:43:10 +0800

    build adjust

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,10 @@ import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'./',
+  build:{
+    outDir:'docs'
+  },
   plugins: [
     vue(),
     VueDevTools(),
